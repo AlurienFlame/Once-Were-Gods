742e62f98546bacbe9ec1d8ed787d6c0fd144c3e
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2021-06-30 07:51:17 +1200

    Added image to card UI

--- Assets/Scripts/PromptCard.cs
+++ Assets/Scripts/PromptCard.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class PromptCard : MonoBehaviour
 {
     public TextMeshProUGUI title;
     public TextMeshProUGUI prompt;
+    public Image image;
 
     public void SetData(PromptOutcomePair data)
     {
         title.text = data.title;
         prompt.text = data.prompt;
+        image.sprite = data.image;
 
     }
 
